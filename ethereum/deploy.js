5f62a88c00567c051be0557e817ca68a0d7ee8bc
Andres Zumelzu <andres.zumelzu@gmail.com>
2018-10-15 22:05:04 -0300

    Fixing out of gas exception

--- ethereum/deploy.js
+++ ethereum/deploy.js
@@ -3,9 +3,9 @@ const Web3 = require('web3');
 const compiledFactory = require('./build/CampaignFactory.json');
 
 const provider = new HDWalletProvider(
-   // Configure infura provider here
-  );
-  
+  '<mnemonic phrase here>',
+  '<infura url here>'
+);
 const web3 = new Web3(provider);
 
 const deploy = async () => {
@@ -13,9 +13,11 @@ const deploy = async () => {
 
   console.log('Attempting to deploy from account', accounts[0]);
 
-  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
-  .deploy({data: '0x' + compiledFactory.bytecode})
-  .send({from: accounts[0]});
+  const result = await new web3.eth.Contract(
+    JSON.parse(compiledFactory.interface)
+  )
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
 
   console.log('Contract deployed to', result.options.address);
 };
---
+++ readme.md
@@ -0,0 +1,5 @@
+Steps to run tests:
+1 - npm install --save
+2 - cd ethereum
+3 - node compile
+4 - npm test
